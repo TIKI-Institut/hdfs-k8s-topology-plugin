be1c762a2f5f869b0e7d58f42f36969377e25921
wobu <buchnerw@googlemail.com>
2020-09-29 13:13:10 +0200

    TIKIDSP-1474 changed to JUnit

--- src/test/java/org/apache/hadoop/net/PodToNodeMappingSuite.java
+++ src/test/java/org/apache/hadoop/net/PodToNodeMappingSuite.java
@@ -1,12 +1,12 @@
 package org.apache.hadoop.net;
 
-import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodBuilder;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.api.model.PodListBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
-import org.junit.Rule;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.net.HttpURLConnection;
@@ -14,12 +14,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PodToNodeMappingSuite {
 
-    public static void runUnitTest() {
+    final private KubernetesServer server = new KubernetesServer();
 
+    @Before
+    public void before() {
+        server.before();
+    }
+
+    @After
+    public void after() {
+        server.after();
+    }
+
+    @Test
+    public void runUnitTest() {
         String[] podIPs = {"10.233.115.144", "10.223.178.167"};
         String[] fqdns = {"kubernetes-worker-5.dsp.tiki.local", "kubernetes-worker-2.dsp.tiki.local"};
 
@@ -27,17 +39,11 @@ public class PodToNodeMappingSuite {
         rawLocalityInfo.addAll(Arrays.asList(podIPs));
         rawLocalityInfo.addAll(Arrays.asList(fqdns));
 
-        PodList expectedPodList = new PodListBuilder().withItems(
-                new PodBuilder().withNewMetadata().withName("pod1").endMetadata().withNewStatus().withPodIP(podIPs[0]).endStatus().withNewSpec().withNodeName("kubernetes-worker-1").endSpec().build(),
-                new PodBuilder().withNewMetadata().withName("pod2").endMetadata().withNewStatus().withPodIP(podIPs[1]).endStatus().withNewSpec().withNodeName("kubernetes-worker-3").endSpec().build()).build();
-
         PodList firstPod = new PodListBuilder().withItems(new PodBuilder().withNewMetadata().withName("pod1").endMetadata().withNewStatus().withPodIP(podIPs[0]).endStatus().withNewSpec().withNodeName("kubernetes-worker-1").endSpec().build()).build();
         PodList secondPod = new PodListBuilder().withItems(new PodBuilder().withNewMetadata().withName("pod2").endMetadata().withNewStatus().withPodIP(podIPs[1]).endStatus().withNewSpec().withNodeName("kubernetes-worker-3").endSpec().build()).build();
 
-        KubernetesServer server = new KubernetesServer();
-        server.before();
-        server.expect().get().withPath("/api/v1/pods?fieldSelector=status.podIP%3D"+podIPs[0]).andReturn(HttpURLConnection.HTTP_OK, firstPod).always();
-        server.expect().get().withPath("/api/v1/pods?fieldSelector=status.podIP%3D"+podIPs[1]).andReturn(HttpURLConnection.HTTP_OK, secondPod).always();
+        server.expect().get().withPath("/api/v1/pods?fieldSelector=status.podIP%3D" + podIPs[0]).andReturn(HttpURLConnection.HTTP_OK, firstPod).always();
+        server.expect().get().withPath("/api/v1/pods?fieldSelector=status.podIP%3D" + podIPs[1]).andReturn(HttpURLConnection.HTTP_OK, secondPod).always();
         KubernetesClient client = server.getClient();
 
         PodToNodeMapping testInstance = new PodToNodeMapping() {
@@ -53,13 +59,9 @@ public class PodToNodeMappingSuite {
         };
 
         assertEquals(new ArrayList<String>(Arrays.asList(netString("kubernetes-worker-1"), netString("kubernetes-worker-3"), netString("kubernetes-worker-5"), netString("kubernetes-worker-2"))), testInstance.resolve(rawLocalityInfo));
-
     }
 
-    public static String netString(String s){return PodToNodeMapping.RACK_NAME + "/" + s;}
-
-    public static void main(String[] args) {
-        PodToNodeMappingSuite.runUnitTest();
+    public static String netString(String s) {
+        return PodToNodeMapping.RACK_NAME + "/" + s;
     }
-
 }
